5a74f4c7605828b367b00423f93a1da5617a9caf
cries <ries_christian@hotmail.com>
2013-04-20 20:23:03 +0200

    Added Comments

--- Prak2/prak2aufg1.c
+++ Prak2/prak2aufg1.c
@@ -1,15 +1,26 @@
+/*
+  File: prak2aufg1.c
+  Author: Christian Ries | mi968
+  Date: 18.04.2013 
+*/
+
 #include <stdio.h>
 #include <string.h>
-
+  
+// argc enthält Anzahl Argumente, *argv[] ist ein Pointer auf die Argumente
 int main (int argc, char *argv[]){
   int i,j;
-  
+  // Anzahl Argumente ausgeben
   printf("%i Argumente gefunden.", argc);
   printf("\n");
+  // Für alle Argumente
   for (i = 0; i < argc; i++){
+    // Argument Nr, Start Adresse in Dezimal & Hexadezimal ausgeben
     printf("Argument %i: %d (%p)\n", i, &(argv[i]), &(argv[i])); 
     printf("--------------------------------\n");
+    // Für jedes Character des Argumentes
     for (j = 0; j < strlen(argv[i]); j++){
+      // Adresse des Zeichen in Dez & Hex ausgeben
       printf("\t Zeichen  %i: %d (%p)- %c\n", j, &(argv[i][j]), &(argv[i][j]), argv[i][j]);
     }
     printf("\n");
--- Prak2/prak2aufg3.c
+++ Prak2/prak2aufg3.c
@@ -1,13 +1,26 @@
+/*
+  File: prak2aufg3.c
+  Author: Christian Ries | mi968
+  Date: 19.04.2013 
+*/
+
 #include <stdio.h>
 #include <unistd.h>
+
 int main() {
   int i, pid, n = 3;
   for(i=0; i<n; i++) {
+    // Prozess erzeugen
     pid = fork();
+    // pid == 0 wenn es ein Kindprozess ist
     if(pid == 0) {
+      // Prozess-ID & Vater-Prozess-ID ausgeben
       printf("Kind %2d: %d von %d\n", i, getpid(), getppid());
+      // Break ausführen wenn im Kindprozess
+      // damit nicht noch weitere Prozesse vom Kindprozess erzeugt werden
       break;
     } else {
+      // Prozess-ID & Vater-Prozess-ID ausgeben
       printf("Vater %2d: %d von %d\n", i, getpid(), getppid());
     }
   }
