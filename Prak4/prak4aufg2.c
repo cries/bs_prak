ffcc3340d758a1b199925826ef3b9cca542b4c7a
cries <ries_christian@hotmail.com>
2013-06-10 14:48:37 +0200

    Updated Aufg2 & 3

--- Prak4/prak4aufg2.c
+++ Prak4/prak4aufg2.c
@@ -7,17 +7,17 @@
 #include <sys/types.h>
 
 #define NUM_OF_CHILDS 3
-#define SEGSIZE sizeof(int)
+#define SEG_SIZE sizeof(int)
 
 void main() {
-  int i, count, id1, id2, *shar_mem1, *shar_mem2;
-  int pid[NUM_OF_CHILDS];
+  int i, pid, count, id1, id2, *shar_mem1, *shar_mem2;
+  int arr_pid[NUM_OF_CHILDS];
   
-  id1 = shmget(IPC_PRIVATE, SEGSIZE, IPC_CREAT|0600);
+  id1 = shmget(IPC_PRIVATE, SEG_SIZE, IPC_CREAT|0600);
   shar_mem1 = (int *)shmat(id1, 0, 0);
   *shar_mem1 = 20;
   
-  id2 = shmget(IPC_PRIVATE, SEGSIZE, IPC_CREAT|0600);
+  id2 = shmget(IPC_PRIVATE, SEG_SIZE, IPC_CREAT|0600);
   shar_mem2 = (int *)shmat(id2, 0, 0);
   *shar_mem2 = 1;
   
@@ -25,38 +25,45 @@ void main() {
   
   
   for (i = 0; i < NUM_OF_CHILDS; i++){
-    pid[i] = fork();
-    if (pid[i] == -1){
+    pid = fork();
+    if (pid == -1){
       printf("Kindprozess konnte nicht erzeugt werden!\n");
       exit(1);
     }
-    if (pid[i]==0){
+    if (pid==0){
       printf("Kind-ID: %d\n", getpid());
       while(1==1){
-        usleep(100000);
+        if (*shar_mem1 > 0){
+          usleep(100000);
+          *shar_mem1 = *shar_mem1 - 1;
+        } else {
+          usleep(100000);
+        }
+
         if (*shar_mem2 == 0) {
           printf("Kennung ist 0\n");
           exit(0);
         }
-        *shar_mem1 = *shar_mem1 - 1;
       }      
+    } else if (pid > 0){
+      arr_pid[i] = pid;
     }  
   }
   
     while(count<60){
         usleep(100000);
+        printf("Lagerbestand: %02d - %02d\n", *shar_mem1, count);
         if (*shar_mem1 <= 0) {
           *shar_mem1 = 20;
           printf("Lagerbestand aufgefuellt!\n");
         }
-        printf("Lagerbestand: %02d - %02d\n", *shar_mem1, count);
         count++;
   }
   
   *shar_mem2 = 0;
   
   for(i = 0; i < NUM_OF_CHILDS; i++){
-    waitpid(pid[i], NULL, 0);
+    waitpid(arr_pid[i], NULL, 0);
   }
   
   printf("Vater-Prozess wird beendet! Shared-Memory wird freigegeben!\n");
--- Prak4/prak4aufg3.c
+++ Prak4/prak4aufg3.c
@@ -11,10 +11,12 @@
 #define SEG_SIZE sizeof(int)
 
 void main() {
-  int i, count, semid, id1, id2, *shar_mem1, *shar_mem2;
-  int pid[NUM_OF_CHILDS];
+  int i, pid, count, semid, id1, id2, *shar_mem1, *shar_mem2;
+  int arr_pid[NUM_OF_CHILDS];
+  unsigned short marker[1];
   struct sembuf semaphore_lock;
   struct sembuf semaphore_unlock;
+  marker[0] = 1;
   
   semaphore_lock.sem_num = 0;
   semaphore_lock.sem_op = -1;
@@ -25,7 +27,7 @@ void main() {
   semaphore_unlock.sem_flg = SEM_UNDO;
   
   semid = semget(IPC_PRIVATE, 1, IPC_CREAT|0600);
-  semctl(semid, 0, SETALL, (int) 1);
+  semctl(semid, 0, SETALL, marker);
   
   id1 = shmget(IPC_PRIVATE, SEG_SIZE, IPC_CREAT|0600);
   shar_mem1 = (int *)shmat(id1, 0, 0);
@@ -38,58 +40,75 @@ void main() {
   
   count = 0;
   
-  
+  // Erzeuge NUM_OF_CHILDS-Anzahl Kinder
   for (i = 0; i < NUM_OF_CHILDS; i++){
-    pid[i] = fork();
-    if (pid[i] == -1){
+    // Kind erzeugen
+    pid = fork();
+    // Kind konnte nicht erzeugt werden
+    if (pid == -1){
       printf("Kindprozess konnte nicht erzeugt werden!\n");
       exit(1);
     }
-    if (pid[i]==0){
-      printf("Kind-ID: %d\n", getpid());
+    // Im Kind-Prozess
+    if (pid==0){
+      printf("Kind (%d) erzeugt!\n", getpid());
+      // Endlosschleife
       while(1){
-        printf("trolol");
-        if(*shar_mem1 > 0) {
+        // Kritischen Bereich sperren
         semop(semid, &semaphore_lock, 1);
+        // Lagerbestand > 0
+        if(*shar_mem1 > 0) {
           usleep(100000);
-          printf("Kind-ID: %d / Lagerbestand: %d\n", i, *shar_mem1);
+          // Lagerbestand um 1 dekrementieren
           *shar_mem1 = *shar_mem1 - 1;
-        semop(semid, &semaphore_unlock, 1); 
         } else {
           usleep(100000);
         }
+        // Kritischen Bereich entsperren
+        semop(semid, &semaphore_unlock, 1); 
         
+        // Kind-Prozess beenden wenn Killsignal gesetzt
         if (*shar_mem2 == 0) {
-          printf("Kennung ist 0\n");
+          printf("Killsignal erhalten! Kind (%d) beendet.\n", getpid());
           exit(0);
         }
       }      
+    } // Vaterprozess sammelt die Kind-PID's
+      else if (pid > 0){
+      arr_pid[i] = pid;  
     }  
   }
-  
+    // Vater-Prozess wiederholt 60 mal 
     while(count<60){
         usleep(100000);
-        printf("Lagerbestand: %02d - %02d\n", *shar_mem1, count);
+        printf("Lagerbestand: %02d\n", *shar_mem1);
+        // semop(semid, &semaphore_lock, 1);
+        // Lagerbestand prüfen 
         if (*shar_mem1 <= 0) {
-//          semop(semid, &semaphore_lock, 1);
+          // Frisch auffüllen
           *shar_mem1 = 20;
-          printf("Lagerbestand aufgefuellt!\n");
-//          semop(semid, &semaphore_unlock, 1); 
+          printf("Lagerbestand aufgefuellt!\n"); 
         }
+        // semop(semid, &semaphore_unlock, 1);
         count++;
-  }
+    }
   
+  // Killsignal setzen
   *shar_mem2 = 0;
   
+  // Auf Kind-Prozesse warten
   for(i = 0; i < NUM_OF_CHILDS; i++){
-    waitpid(pid[i], NULL, 0);
+    waitpid(arr_pid[i], NULL, 0);
   }
   
   printf("Vater-Prozess wird beendet! Shared-Memory wird freigegeben!\n");
   
+  // Shared Memory vom Speicherbereich lösen
   shmdt(shar_mem1);
   shmdt(shar_mem2);
+  // Shared Memory wieder freigeben (löschen)
   shmctl(id1, IPC_RMID, 0);
   shmctl(id2, IPC_RMID, 0);
+  // Semaphore löschen
   semctl(semid, 0, IPC_RMID, 0);
 }
